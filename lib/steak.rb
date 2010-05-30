986923121f96c380f64c0f714351e5ea39ac858b
Luismi Cavalle <luismi@lmcavalle.com>
2010-05-30 22:19:39 +0200

    Removing rails integration testing stuff, since with capybara and rack-test, it's no longer needed

--- lib/steak.rb
+++ lib/steak.rb
@@ -11,22 +11,3 @@ end
 module Rspec::Core::ObjectExtensions
   alias feature describe
 end
-
-if defined?(Rspec::Rails)
-
-  module Rspec::Rails::Example
-    module AcceptanceExampleGroup
-      def method_missing(sym, *args, &block)
-        return Rspec::Matchers::Be.new(sym, *args)  if sym.to_s =~ /^be_/
-        return Rspec::Matchers::Has.new(sym, *args) if sym.to_s =~ /^have_/
-        super
-      end
-      
-      Rspec.configure do |c|
-        c.include RequestExampleGroupBehaviour, :example_group => { :file_path => /\bspec\/acceptance\// }
-        c.include self, :example_group => { :file_path => /\bspec\/acceptance\// }
-      end
-    end
-  end
-  
-end
